e38550282931e681e5dc1e1a783bcdeaa9927be8
zeroone001 <ilylys@163.com>
2019-11-05 13:36:36 +0800

    update

---
+++ ES6-Array/index.js
@@ -0,0 +1,14 @@
+ // 高阶函数
+ /*
+    1. map,
+    @params: Fun(item, index, array)
+    返回新的数组
+*/
+var arr = [];
+arr.map((item, index, array) => {
+    return item + 1;
+});
+/*
+    2. reduce
+    @params : Fun()
+*/
