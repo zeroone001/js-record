935e7e4c0f94c379da831b5cdecbe4cd4e16ae58
zeroone001 <ilylys@163.com>
2019-11-09 09:57:15 +0800

    map

--- ES6-Array/index.js
+++ ES6-Array/index.js
@@ -8,6 +8,17 @@ const arr = [1,2,3,5,63,3];
 var newArr = arr.map((item, index, array) => {
     return item + 1;
 });
+
+['1', '2', '3'].map(parseInt); // 1, NaN, NaN
+// 因为parseInt接受两个参数， parseInt 经常被带着一个参数使用, 却消耗两个。第一个参数是一个表达式而第二个是callback function的基,
+// Array.prototype.map 传递3个参数:
+// 下面是解决方案
+function returnInt(element) {
+    return parseInt(element, 10);
+}
+
+['1', '2', '3'].map(returnInt); // [1, 2, 3]
+
 /*
     2. reduce
     @params : Fun(preNum, item, index, array)
