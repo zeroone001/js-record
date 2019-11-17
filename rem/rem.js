e1ed55c33d701ca5fa5b4e48fe8d35aa89742784
zeroone001 <ilylys@163.com>
2019-11-17 11:13:54 +0800

    update

--- rem/rem.js
+++ rem/rem.js
@@ -1,3 +1,10 @@
+/* 
+    100 是比例，750是设计稿尺寸
+    最后的rem, $n 是设计稿尺寸
+    @function rem ($n) {
+        @return $n/100 * 1rem;
+    }
+*/
 (function (w) {
     var d = document.documentElement;
     var fun = function () {
