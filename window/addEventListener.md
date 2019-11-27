fcb0fd391b4a8040cd94f9bbb60f11184ef20a1a
zeroone001 <ilylys@163.com>
2019-11-27 15:46:00 +0800

    update

---
+++ window/addEventListener.md
@@ -0,0 +1,4 @@
+[stackoverflow](https://stackoverflow.com/questions/12045440/difference-between-document-addeventlistener-and-window-addeventlistener)
+
+1. Difference between document.addEventListener and window.addEventListener?
+
--- 常用功能/index.js
+++ 常用功能/index.js
@@ -7,10 +7,20 @@ function lazyLoad () {
     let scrollTop = document.documentElement.scrollTop;
 
     domImgArr.forEach(function (ele, index) {
-
+        // offsetTop 是相对于父元素的，下面的一个会更好一些
+        var domTop = ele.getBoundingClientRect().top;
+        if (domTop < screenHeight + scrollTop) {
+            if (ele.getAttribute('src') === 'default.jpg') {
+                var _src = ele.getAttribute('data-src');
+                ele.src = _src;
+            }
+        }
     });
-
-    if () {
-
-    }
+}
+window.addEventListener('scroll', function () {
+    lazyLoad();
+});
+// 函数节流来进行优化
+function throttle () {
+    
 }
