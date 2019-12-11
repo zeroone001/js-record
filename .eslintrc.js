b931140b14c089fc2e5c79cd1f4ccb8dc6c63850
zeroone001 <ilylys@163.com>
2019-12-11 21:41:04 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,41 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    'standard'
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module'
-  },
-  plugins: [
-    'vue',
-    '@typescript-eslint'
-  ],
-  rules: {
-  }
+    root: true, // eslint 认为这就是根目录，不会再向上查找
+    env: {
+        browser: true,
+        es6: true,
+        jquery: true,
+        commonjs: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        'standard', // eslint-config-standard
+        'html'
+    ],
+    globals: {
+        '$': true, // true表示该变量为 writeable，而 false 表示 readonly
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    // 解析器配置参数
+    parserOptions: {
+        ecmaVersion: 2018,
+        parser: 'babel-eslint', // 也可以 @typescript-eslint/parser
+        sourceType: 'module' // 代码类型
+    },
+    plugins: [
+        'vue',
+        '@typescript-eslint',
+        'import', // eslint-plugin-import 该插件想要支持对ES2015+ (ES6+) import/export语法的校验, 并防止一些文件路径拼错或者是导入名称错误的情况
+        'promise' // promise规范写法检查插件，附带了一些校验规则
+    ],
+    rules: {
+        // allow paren-less arrow functions
+        'arrow-parens': 0,
+        // allow async-await
+        'generator-star-spacing': 0,
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        'indent': ['error', 4],
+        'semi': ['error', 'always']
+    }
 }
---
+++ Eslint/README.md
@@ -0,0 +1,15 @@
+###### eslint 学习记录
+
+[https://juejin.im/post/5d3d3a685188257206519148](https://juejin.im/post/5d3d3a685188257206519148)
+
+1. 环境与全局变量
+
+`env` `globals`
+
+2. 解析器配置
+
+`parse` `parseOptions`
+
+3. rules
+
+4. extends
