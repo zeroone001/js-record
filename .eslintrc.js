e3c8b24f29811743aaa4c32ee7ba2f183f19bfe2
zeroone001 <ilylys@163.com>
2019-12-13 16:17:00 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,18 +6,6 @@ module.exports = {
         jquery: true,
         commonjs: true
     },
-    extends: [
-        'plugin:vue/base',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:vue/recommended',
-        'standard', // eslint-config-standard
-        'html'
-    ],
-    globals: {
-        '$': true, // true表示该变量为 writeable，而 false 表示 readonly
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly'
-    },
     parser: 'vue-eslint-parser',
     // 解析器配置参数
     parserOptions: {
@@ -25,12 +13,24 @@ module.exports = {
         parser: 'babel-eslint', // 也可以 @typescript-eslint/parser
         sourceType: 'module' // 代码类型
     },
+    extends: [
+        'plugin:vue/base',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/recommended',
+        'standard', // eslint-config-standard
+        'html'
+    ],
     plugins: [
         'vue',
         '@typescript-eslint',
         'import', // eslint-plugin-import 该插件想要支持对ES2015+ (ES6+) import/export语法的校验, 并防止一些文件路径拼错或者是导入名称错误的情况
         'promise' // promise规范写法检查插件，附带了一些校验规则
     ],
+    globals: {
+        '$': 'writeable', // true表示该变量为 writeable，而 false 表示 readonly, true,false 是历史原因
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
     rules: {
         // allow paren-less arrow functions
         'arrow-parens': 0,
@@ -39,6 +39,9 @@ module.exports = {
         // allow debugger during development
         'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
         'indent': ['error', 4],
-        'semi': ['error', 'always']
+        'semi': ['error', 'always'],
+        'vue/html-indent': ['error', 4],
+        'vue/html-self-closing': 0,
+        'vue/html-closing-bracket-newline': 0
     }
 }
--- Eslint/README.md
+++ Eslint/README.md
@@ -1,27 +1,46 @@
-###### eslint 学习记录
+#### eslint 学习记录
 
-[https://juejin.im/post/5d3d3a685188257206519148](https://juejin.im/post/5d3d3a685188257206519148)
-[https://juejin.im/post/5ce3dbd1e51d454f72302461](https://juejin.im/post/5ce3dbd1e51d454f72302461)
+[掘金](https://juejin.im/post/5d3d3a685188257206519148)
+[掘金](https://juejin.im/post/5ce3dbd1e51d454f72302461)
+[官网](https://cn.eslint.org/docs/user-guide/configuring)
 
 
 1. 环境与全局变量
 
 `env` `globals`
+```
+{
+	"globals": {
+		"var1": "writeable",
+		"var2": "readonly"
+	}
+}
+```
 
 2. 解析器配置
    
-    // If you want to use custom parsers such as babel-eslint or @typescript-eslint/parser, you have to use parserOptions.parser option instead of parser option.
-
+// If you want to use custom parsers such as babel-eslint or @typescript-eslint/parser, you have to use parserOptions.parser option instead of parser option.
 
 `parse` `parseOptions`
 
-1. rules
+
+3. rules
 
 > [官方rules](https://cn.eslint.org/docs/rules/)
 
-1. extends
+4. extends
 
-2. webpack
+```
+{
+	"extends": [
+		"plugin:vue/recommended",
+		"plugin:@typescript-eslint/recommended",
+		"standard"
+	]
+}
+```
+5. webpack
+
 ```js
 devServer: {
     overlay: true // 如果代码不符合规范，就会在浏览器上面加上一个蒙层
@@ -41,6 +60,7 @@ use: [
 ```
 
 加入用happypack， 配置如下
+
 ```js
 var HappyPack = require('happypack');
 const os = require('os');
@@ -65,6 +85,31 @@ plugins: [
     })
 ]
 ```
-3. plugins
+6. plugins
 
-`eslint-plugin-vue`
+```
+{
+	"plugins": [
+		"vue"
+	]
+}
+```
+
+7. 单独配置一组文件的规则
+
+```js
+{
+	"overrides": {
+		"files": ["*.a.js"],
+		"rules": {
+			"no-unused-expressions": "off"
+		}
+	}
+}
+```
+
+#### 命令行修复
+
+```js
+npx eslint --fix "src/..."
+```
