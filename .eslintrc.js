aede76a71ccbd18f6375537edf52c6426c1916f5
zeroone001 <ilylys@163.com>
2019-12-16 14:30:39 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,51 @@
 module.exports = {
-    root: true, // eslint 认为这就是根目录，不会再向上查找
-    env: {
-        browser: true,
-        es6: true,
-        jquery: true,
-        commonjs: true
+	'env': {
+		'browser': true,
+		'es6': true,
+		'node': true
+	},
+	'extends': [
+        'eslint:recommended',
+        'plugin:vue/recommended'
+	],
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
     },
-    parser: 'vue-eslint-parser',
-    // 解析器配置参数
-    parserOptions: {
-        ecmaVersion: 2018,
-        parser: 'babel-eslint', // 也可以 @typescript-eslint/parser
-        sourceType: 'module' // 代码类型
-    },
-    extends: [
-        'plugin:vue/base',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:vue/recommended',
-        'standard', // eslint-config-standard
-        'html'
-    ],
-    plugins: [
-        'vue',
-        '@typescript-eslint',
-        'import', // eslint-plugin-import 该插件想要支持对ES2015+ (ES6+) import/export语法的校验, 并防止一些文件路径拼错或者是导入名称错误的情况
-        'promise' // promise规范写法检查插件，附带了一些校验规则
-    ],
-    globals: {
-        '$': 'writeable', // true表示该变量为 writeable，而 false 表示 readonly, true,false 是历史原因
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly'
-    },
-    rules: {
-        // allow paren-less arrow functions
-        'arrow-parens': 0,
-        // allow async-await
+    'parser': 'vue-eslint-parser',
+	'parserOptions': {
+		'parser': 'babel-eslint',
+		'ecmaVersion': 2018,
+		'sourceType': 'module'
+	},
+	'plugins': [
+        'import',
+        'html',
+        'vue'
+	],
+	'rules': {
+		'vue/singleline-html-element-content-newline': 'off',
+        'no-unused-vars': 0,
         'generator-star-spacing': 0,
-        // allow debugger during development
-        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-        'indent': ['error', 4],
-        'semi': ['error', 'always'],
+		'no-dupe-keys': 0,
+		'indent': ['warn', 4],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+        ],
         'vue/html-indent': ['error', 4],
-        'vue/html-self-closing': 0,
+        'vue/max-attributes-per-line': 'off',
+        'vue/order-in-components': 'off',
+        'vue/html-end-tags': 'off',
+        'vue/html-self-closing': 'off',
         'vue/html-closing-bracket-newline': 0
-    }
-}
+	}
+};
