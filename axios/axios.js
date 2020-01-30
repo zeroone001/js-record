9693e21cee078031cf2e71c8a485af1117cbfd7d
zeroone001 <ilylys@163.com>
2020-01-30 12:53:12 +0800

    update

--- axios/axios.js
+++ axios/axios.js
@@ -1,4 +1,4 @@
-```javascript
+// 对axios的一个封装
 import axios from 'axios'
 /* eslint-disable */
 const service = axios.create({
@@ -59,11 +59,4 @@ export default {
       })
     }
 }
-```
-
-###### 注意
-
-* (一) 当前端配置withCredentials=true时, 后端配置Access-Control-Allow-Origin不能为*, 必须是相应地址
-* (二) 当配置withCredentials=true时, 后端需配置Access-Control-Allow-Credentials
-* (三) 当前端配置请求头时, 后端需要配置Access-Control-Allow-Headers为对应的请求头集合
 
--- 常用功能/README.md
+++ 常用功能/README.md
@@ -1,5 +1,17 @@
 #### 浏览器复制粘贴
 
+```js
+// 动态添加input的方法
+function copy (id) {
+    var t = document.createElement('input');
+    t.setAttribute('value', document.getElementById(id).innerHTML);
+    document.body.appendChild(t);
+    t.select();
+    var content = window.getSelection().toString();
+    document.execCommand('copy');
+    document.body.removeChild(t);
+}
 
+```
 
 
